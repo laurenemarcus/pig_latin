6aa5d3e457fda9d36432daeed8ce3884e165a44c
Lauren <lauren.e.marcus@gmail.com>
2015-02-10 16:55:59 -0800

    Account for words that start with two consonants.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 var pigLatin = function(word) {
-  var vowels = ["a", "e", "i", "o", "u", "y"];
-  var idx = vowels.indexOf(word.slice(0,1));
+  var vowels = ["a", "e", "i", "o", "u"];
+  var firstLetter = vowels.indexOf(word.slice(0,1));
+  var secondLetter = vowels.indexOf(word.slice(1,2));
   var letterize = word.split("");
-  if (idx === -1) {
+  if ((firstLetter === -1) && (secondLetter != -1)) {
       return letterize.concat(word.slice(0,1)).concat("ay").join("").substr(1);
+  } else if ((firstLetter === -1) && (secondLetter === -1)) {
+      return letterize.concat(word.slice(0,2)).concat("ay").join("").substr(2);
   } else {
     return word + "ay";
   }
